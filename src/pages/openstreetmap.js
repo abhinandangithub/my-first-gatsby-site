645f48e5b379c002e0720686416ad7d7fe52b726
Abhinandan <abhinandangp.pes29@gmail.com>
2022-09-09 12:35:06 +0530

    adding  working openstreet map

--- src/pages/openstreetmap.js
+++ src/pages/openstreetmap.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup  } from 'react-leaflet'
-import "./openstreetmap";
+import { MapContainer, TileLayer, Marker, Popup, Map, LeafletMap } from 'react-leaflet'
+import "../assets/styles/openstreetmap.css";
+// import "../assets/styles/openstreetmap_1.css";
 
 const OpenStreetMap = () => {
     return (
-        <MapContainer center={[10.99835602, 77.01502627]} zoom={7} >
+        <MapContainer center={[10.99835602, 77.01502627]} zoom={3} >
 
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -16,6 +17,17 @@ const OpenStreetMap = () => {
                 </Popup>
             </Marker>
         </MapContainer>
+        // <Map center={[51, -1]} zoom={3}>
+        //     <TileLayer
+        //         url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        //         attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        //     />
+        //     <Marker position={[10.99835602, 77.01502627]}>
+        //         <Popup>
+        //             I am a pop-up!
+        //         </Popup>
+        //     </Marker>
+        // </Map>
     );
 }
 
